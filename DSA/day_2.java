9861b7b225f75a7ad81071c5f55dd351c4cf1a64
Shailsharma2604 <shailsharma020604@gmail.com>
2025-01-23 11:31:30 +0530

    1

--- DSA/day_2.java
+++ DSA/day_2.java
@@ -1,5 +1,8 @@
 public class day_2 {
     public static void main(String[] args) {
-        
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 }
